e65a385ddf39c9fc611e9f6fe85501d306846ba8
EverCastillo <euca1000@gmail.com>
2020-09-17 11:51:39 -0500

    Cambiar de rol sin hacer logout

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -31,6 +31,10 @@ $(document).ready(function (){
         }
         ajaxRequest(data, '/ajax-sesion', 'asignar-rol');
     });
+    $('.cambiar-rol').on('click', function(event){
+        event.preventDefault();
+        modal.modal('show');
+    });
 
     function ajaxRequest(data, url, funcion){
         $.ajax({
